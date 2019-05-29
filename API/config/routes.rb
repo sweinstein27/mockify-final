d5d9ceb134df8358a42b44f55867e2a8b98dc487
sweinstein27 <sweinstein27@gmail.com>
2019-05-29 09:44:22 -0700

    added api routes

--- API/config/routes.rb
+++ API/config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-
-  root "static_pages#root"
-
-  namespace :api, defaults: {format: :json} do
-    resources :users, only: [:create, :update, :index] do
-     
-    end
-    resource :session, only: [:create, :destroy, :show]
-    
-
-    end
-end
+  namespace :api do
+    resources :searches
+  end
+ end
